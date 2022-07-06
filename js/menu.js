8ac98aa74dc89ee458053a0bc36e954a5e438169
Татьяна Брыкова <tatanabrykova@MacBook-Pro-Tatana.local>
2022-07-06 12:12:57 -0500

    Домашнее задание 3 неделя

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+const openMenu = document.querySelector("#openOverlay");
+const body = document.body;
+const create = createModal();
+
+openMenu.addEventListener("click", e => {
+  e.preventDefault();
+  body.appendChild(create);
+})
+
+function createModal () {
+  const overlayElement = document.createElement("div");
+  overlayElement.classList.add("overlay"); 
+
+  const template = document.querySelector("#overlayTemplate");
+
+  overlayElement.innerHTML = template.innerHTML; 
+
+  const closeElement = overlayElement.querySelector("#close");
+
+  closeElement.addEventListener("click", e => {
+    e.preventDefault();
+    body.removeChild(overlayElement);
+  }
+  )
+
+  return overlayElement;
+}
